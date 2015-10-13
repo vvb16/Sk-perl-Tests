8d0d605876584a5166c4d4b9ac85296a3c61778b
Vitaly Belekhov <rmrf17@gmail.com>
2015-10-13 23:02:11 +0600

    Fixed runtests.pl for current test suite. Renamed $test to more common $self. Removed unused imports.

--- tests/VVB/Test/SK/Base.pm
+++ tests/VVB/Test/SK/Base.pm
@@ -7,37 +7,34 @@ use base 'VVB::Test::Base';
 use Test::Most;
 use IXXAT::VCI3 qw(pvciFormatError readMessage sendMessage);
 use Scalar::Util qw(dualvar);
-use List::Util qw(min);
-use Time::HiRes;
-use POSIX;
 
 VVB::Test::SK::Base->SKIP_CLASS( 1 );
 
 sub set_device_id
 {
-  my $test = shift;
-  $test->{device_id} = shift;
+  my $self = shift;
+  $self->{device_id} = shift;
 }
 
 sub reset_cmd_count
 {
-  my $test = shift;
-  $test->{cmd_count} = 0;
+  my $self = shift;
+  $self->{cmd_count} = 0;
 }
 
 sub SK_command_execute
 {
-  my $test = shift;
+  my $self = shift;
   my ($timeout, $command, $data) = @_;
   my $result;
   my $response = {};
 
   return dualvar(-1, "too long command: " . length($data)) if length($data) > 6;
 
-  $test->{cmd_count} = ($test->{cmd_count} + 1) & 0xFF;
+  $self->{cmd_count} = ($self->{cmd_count} + 1) & 0xFF;
   $result = sendMessage($VVB::Test::Base::channel, $timeout,
-    $test->{device_id}, length($data) + 2,
-    pack('C2', $test->{cmd_count}, $command) . $data);
+    $self->{device_id}, length($data) + 2,
+    pack('C2', $self->{cmd_count}, $command) . $data);
   return dualvar(-1, "sendMessage error: " . pvciFormatError($result)) if $result != 0;
 
   $result = readMessage($VVB::Test::Base::channel, $timeout, $response);
@@ -45,7 +42,7 @@ sub SK_command_execute
   return dualvar(-1, "answer type should be 0 (DATA) got " . $response->{type}) if $response->{type} != 0;
   return dualvar(-1, "answer length should be at least 2 got " . length($response->{data})) if length($response->{data}) < 2;
   my ($cmd_count, $cmd, $resp_data) = unpack('C2a*', $response->{data});
-  return dualvar(-1, sprintf("answer command counter should be %d got %d", $test->{cmd_count}, $cmd_count)) if $cmd_count != $test->{cmd_count};
+  return dualvar(-1, sprintf("answer command counter should be %d got %d", $self->{cmd_count}, $cmd_count)) if $cmd_count != $self->{cmd_count};
   return dualvar(-1, sprintf("answer command should be 0x%X (RESPONSE_OK) got 0x%X", ($command | 0x80), $cmd)) if $cmd != ($command | 0x80);
 
   return dualvar(0, $resp_data);
@@ -53,14 +50,14 @@ sub SK_command_execute
 
 sub check_CAN_channel #: Tests(1)
 {
-  my $test = shift;
+  my $self = shift;
   die_on_fail;
   ok(defined $VVB::Test::Base::channel, "CAN channel connection");
 }
 
 sub find_devices #: Tests(3)
 {
-  my $test = shift;
+  my $self = shift;
   my $response = {};
 
   my $command = pack('C*', 0);
@@ -77,32 +74,32 @@ sub find_devices #: Tests(3)
     my ($code, $id, $serial) = unpack('CS2', $response->{data});
     return dualvar(-1, "answer code should be 128 (RESPONSE_OK) got " . $code) if $code != 0x80;
     diag sprintf("Found device ID: 0x%X\tSerial: %d", $id, $serial);
-    push $test->{devices}, {id => $id, serial => $serial};
+    push $self->{devices}, {id => $id, serial => $serial};
   }
   return dualvar(0, "");
 }
 
 sub startup : Tests(startup => 3)
 {
-  my $test = shift;
+  my $self = shift;
   die_on_fail;
 
   SKIP: {
-    skip "Already initialized", 3 if $test->{devices};
-    $test->{devices} = [];
-    $test->SUPER::startup();
-    $test->check_CAN_channel();
-    my $result = $test->find_devices();
+    skip "Already initialized", 3 if $self->{devices};
+    $self->{devices} = [];
+    $self->SUPER::startup();
+    $self->check_CAN_channel();
+    my $result = $self->find_devices();
     cmp_ok($result, '==', 0, "find_devices should succeed") || diag $result;
-    cmp_ok(scalar(@{$test->{devices}}), '>', 0, "at least one device should present");
+    cmp_ok(scalar(@{$self->{devices}}), '>', 0, "at least one device should present");
   }
 }
 
 sub t05_SW_VER : Tests(2)
 {
-  my $test = shift;
+  my $self = shift;
 
-  my $result = $test->SK_command_execute(100, 0x00, '');
+  my $result = $self->SK_command_execute(100, 0x00, '');
   cmp_ok($result, '==', 0, "command SW_VER should succeed") || diag $result;
   is(length($result), 4, "answer length of SW_VER should be 4");
   diag sprintf("SW_VER: 0x%X", unpack('L', $result));
@@ -110,7 +107,7 @@ sub t05_SW_VER : Tests(2)
 
 sub t06_runtime_stats : Tests(21)
 {
-  my $test = shift;
+  my $self = shift;
   my @dict = ("power cycles",
     "total time",
     "time at t < 0",
@@ -122,7 +119,7 @@ sub t06_runtime_stats : Tests(21)
   my $code = 0;
   for my $descr (@dict)
   {
-    my $result = $test->SK_command_execute(100, 0x01, pack('C', $code));
+    my $result = $self->SK_command_execute(100, 0x01, pack('C', $code));
     cmp_ok($result, '==', 0, "command runtime_stats should succeed") || diag $result;
     is(length($result), 5, "answer length of runtime_stats should be 5");
     my ($resp_code, $duration) = unpack('CL', $result);
--- tests/VVB/Test/SK/Pulser.pm
+++ tests/VVB/Test/SK/Pulser.pm
@@ -5,27 +5,23 @@ package VVB::Test::SK::Pulser;
 use base 'VVB::Test::SK::Base';
 
 use Test::Most;
-use Scalar::Util qw(dualvar);
-use List::Util qw(min);
-use Time::HiRes;
-use POSIX;
 
 sub _t01_Pulser_found : Tests(1)
 {
-  my $test = shift;
+  my $self = shift;
   die_on_fail;
 
-  my @pulsers = grep { $_->{id} == 0x10 } @{$test->{devices}};
+  my @pulsers = grep { $_->{id} == 0x10 } @{$self->{devices}};
   is(scalar @pulsers, 1, "Exactly one Pulser should be found");
-  $test->set_device_id(0x10); 
-  $test->reset_cmd_count(); 
+  $self->set_device_id(0x10); 
+  $self->reset_cmd_count(); 
 }
 
 sub t10_device_default_mode : Tests(3)
 {
-  my $test = shift;
+  my $self = shift;
 
-  my $result = $test->SK_command_execute(100, 0x20, pack('C2', 0, 0));
+  my $result = $self->SK_command_execute(100, 0x20, pack('C2', 0, 0));
   cmp_ok($result, '==', 0, "command 0x06 should succeed") || diag $result;
   is(length($result), 1, "answer length of 0x20 should be 1");
   my ($resp) = unpack('C', $result);
@@ -38,10 +34,10 @@ sub t10_device_default_mode : Tests(3)
 
 sub t11_device_status : Tests(4)
 {
-  my $test = shift;
+  my $self = shift;
   my $code = 1;
 
-    my $result = $test->SK_command_execute(100, 0x06, pack('C1', $code));
+    my $result = $self->SK_command_execute(100, 0x06, pack('C1', $code));
     cmp_ok($result, '==', 0, "command 0x06 should succeed") || diag $result;
     is(length($result), 3, "answer length of 0x06 should be 3");
     my ($resp_code, $resp) = unpack('CS', $result);
@@ -52,11 +48,11 @@ sub t11_device_status : Tests(4)
 
 sub t20_clear_mode : Tests(12)
 {
-  my $test = shift;
+  my $self = shift;
 
   for my $mode (0..1)
   {
-    my $result = $test->SK_command_execute(100, 0x23, pack('C2x1', 1, $mode));
+    my $result = $self->SK_command_execute(100, 0x23, pack('C2x1', 1, $mode));
     cmp_ok($result, '==', 0, "command 0x23 should succeed") || diag $result;
     is(length($result), 3, "answer length of 0x23 should be 3");
     my ($resp_mode, $resp_pack, $resp_pos) = unpack('C3', $result);
@@ -66,7 +62,7 @@ sub t20_clear_mode : Tests(12)
     TODO: {
     local $TODO = "Possibly not implemented in device";
 
-    my $result = $test->SK_command_execute(100, 0x25, pack('C2', $mode, 0));
+    my $result = $self->SK_command_execute(100, 0x25, pack('C2', $mode, 0));
     cmp_ok($result, '==', 0, "command 0x25 should succeed") || diag $result;
     is(length($result), 3, "answer length of 0x25 should be 3");
     my ($resp_mode, $resp_pack, $resp_is_end) = unpack('C3', $result);
